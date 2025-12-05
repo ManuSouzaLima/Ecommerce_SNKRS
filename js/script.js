30a0982bf4bee039d6d3c79b974ad1bd7b89e518
Emanuel Souza <111304925+ManuSouzaLima@users.noreply.github.com>
2025-12-04 23:19:54 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const btn_login = document.querySelector(".login_btn")
+const btn_cadastro = document.querySelector(".cadastrar_btn")
+const modal_login = document.getElementById("modal_login")
+
+btn_login.addEventListener("click", ()=>{
+    console.log("funciona")
+})
+
+   
